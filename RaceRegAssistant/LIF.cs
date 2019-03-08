08df5d2b47f5875e03d98b83ce394a51f6703744
Jackson Porter <jackson_porter@outlook.com>
2019-03-08 11:15:15 -0700

    Added additional files from old project.

---
+++ RaceRegAssistant/LIF.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using LIFFileViewer.Model;
+
+namespace LIFFileViewer.LIFTools
+{
+    public class LIF : INotifyPropertyChanged
+    {
+        private ObservableCollection<Entry> entries;
+        public ObservableCollection<Entry> Entries
+        {
+            get
+            {
+                return entries;
+            }
+            set
+            {
+                entries = value;
+                OnPropertyChanged(nameof(Entries));
+            }
+        }
+
+        private Entry selectedEntry;
+        public Entry SelectedEntry
+        {
+            get
+            {
+                return selectedEntry;
+            }
+            set
+            {
+                selectedEntry = value;
+                OnPropertyChanged(nameof(SelectedEntry));
+            }
+        }
+
+        public int EventId { get; internal set; }
+        public int RoundId { get; internal set; }
+        public int HeatId { get; internal set; }
+        public string EventName { get; internal set; }
+        public int EventDistance { get; internal set; }
+        public DateTime StartTime { get; internal set; }
+
+        public LIF()
+        {
+            entries = new ObservableCollection<Entry>();
+
+            //var tempEntry = new Entry();
+            //tempEntry.Place = 1;
+            //tempEntry.RacerId = 0001;
+            //tempEntry.Lane = 1;
+            //tempEntry.LastName = "Porter";
+            //tempEntry.FirstName = "Jackson";
+            //tempEntry.Affiliation = "SC";
+            //tempEntry.Time = DateTime.Now;
+
+            //entries.Add(tempEntry);
+        }
+
+        internal void AddEntry(Entry entry)
+        {
+            entries.Add(entry);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RaceRegAssistant/LIFReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using LIFFileViewer.Model;
+
+namespace LIFFileViewer.LIFTools
+{
+    public class LIFReader
+    {
+        private string lifFILE;
+        public LIF lif;
+
+        public LIFReader(string lifFILE)
+        {
+            this.lifFILE = lifFILE;
+            lif = new LIF();
+
+            AnalyzeAndLoad();
+        }
+
+        private async void AnalyzeAndLoad()
+        {
+            string[] lines = await System.IO.File.ReadAllLinesAsync(lifFILE);
+
+            LoadFirstLine(lines[0]);
+            LoadEntries(lines);
+        }
+
+        private void LoadFirstLine(string firstLine)
+        {
+            string[] split = firstLine.Split(',');
+            lif.EventId = int.Parse(split[0]);
+            lif.RoundId = int.Parse(split[1]);
+            lif.HeatId = int.Parse(split[2]);
+            lif.EventName = split[3];
+            lif.EventDistance = int.Parse(split[9]);
+            lif.StartTime = DateTime.Parse(split[10]);
+        }
+
+        private void LoadEntries(string[] lines)
+        {
+            for(int i = 1; i < lines.Length; i++)
+            {
+                string[] split = lines[i].Split(',');
+
+                Entry entry = new Entry();
+                entry.Place = int.Parse(split[0]);
+                entry.RacerId = int.Parse(split[1]);
+                entry.Lane = int.Parse(split[2]);
+                entry.LastName = split[3];
+                entry.FirstName = split[4];
+                entry.Affiliation = split[5];
+                //entry.Time = DateTime.Parse(split[6]);
+                entry.Time = split[6];
+
+                lif.AddEntry(entry);
+            }
+        }
+
+        public LIF GetLIFObject()
+        {
+            return this.lif;
+        }
+    }
+}
